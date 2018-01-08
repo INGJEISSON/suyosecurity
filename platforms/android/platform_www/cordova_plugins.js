841541f2b933c3dedb556d6861eaa99bb007cd22
jeisson <jibarguen@suyo.co>
2018-01-08 08:17:45 -0500

    Versión 1.0 beta Suyo Security

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,47 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
-    "clobbers": [
-      "OneSignal"
-    ]
-  },
-  {
-    "id": "cordova-plugin-inappbrowser.inappbrowser",
-    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-    "pluginId": "cordova-plugin-inappbrowser",
-    "clobbers": [
-      "cordova.InAppBrowser.open",
-      "window.open"
-    ]
-  },
-  {
-    "id": "cordova-universal-links-plugin.universalLinks",
-    "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
-    "pluginId": "cordova-universal-links-plugin",
-    "clobbers": [
-      "universalLinks"
-    ]
-  },
-  {
-    "id": "cordova-plugin-browsertab.BrowserTab",
-    "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
-    "pluginId": "cordova-plugin-browsertab",
-    "clobbers": [
-      "cordova.plugins.browsertab"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "onesignal-cordova-plugin": "2.2.5",
-  "cordova-plugin-inappbrowser": "2.0.1",
-  "cordova-universal-links-plugin": "1.2.1",
-  "cordova-plugin-browsertab": "0.2.0"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,38 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-        "id": "onesignal-cordova-plugin.OneSignal",
-        "pluginId": "onesignal-cordova-plugin",
-        "clobbers": [
-            "OneSignal"
-        ]
-    },
-    {
-        "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
-        "id": "cordova-universal-links-plugin.universalLinks",
-        "pluginId": "cordova-universal-links-plugin",
-        "clobbers": [
-            "universalLinks"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
-        "id": "cordova-plugin-browsertab.BrowserTab",
-        "pluginId": "cordova-plugin-browsertab",
-        "clobbers": [
-            "cordova.plugins.browsertab"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3",
-    "onesignal-cordova-plugin": "2.2.5",
-    "cordova-universal-links-plugin": "1.2.1",
-    "cordova-plugin-compat": "1.2.0",
-    "cordova-plugin-browsertab": "0.2.0"
+    "cordova-plugin-whitelist": "1.3.3"
 }
 // BOTTOM OF METADATA
 });
