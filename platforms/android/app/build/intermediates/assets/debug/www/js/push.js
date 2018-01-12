955c9d17299024fadfefd5265ae09c571a389578
jeisson <jibarguen@suyo.co>
2018-01-12 15:29:21 -0500

    Recibir dato de push

--- platforms/android/app/build/intermediates/assets/debug/www/js/push.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/push.js
@@ -1,11 +1,53 @@
-document.addEventListener('deviceready', function () {
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
 
-  window.plugins.OneSignal
-    .startInit("626ad5c9-9de8-4384-a12b-9c3aec240461")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+
+        // OneSignal Initialization
+        // Enable to debug issues.
+        // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
-}, false);
+        // Set your iOS Settings
+        var iosSettings = {};
+        iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
+
+        window.plugins.OneSignal
+          .startInit("626ad5c9-9de8-4384-a12b-9c3aec240461")
+          .handleNotificationReceived(function(jsonData) {
+            alert("Notification received: \n" + JSON.stringify(jsonData));
+            console.log('Did I receive a notification: ' + JSON.stringify(jsonData));
+          })
+          .handleNotificationOpened(function(jsonData) {
+            alert("Notification opened: \n" + JSON.stringify(jsonData));
+            console.log('didOpenRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+          })
+          .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.InAppAlert)
+          .iOSSettings(iosSettings)
+          .endInit();
+        
+        //Call syncHashedEmail anywhere in your app if you have the user's email.
+        //This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+        //window.plugins.OneSignal.syncHashedEmail(userEmail);
+    }
+};
